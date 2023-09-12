93b769047b70718434b579f6690688c2bcf1784d
Logan Copeland <logankcc@gmail.com>
2023-09-12 18:10:23 -0400

    Download MNIST

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+train_transform = transforms.Compose([transforms.ToTensor()])
+train_set = torchvision.datasets.MNIST('./data/mnist', train=True, download=True, transform=train_transform)
